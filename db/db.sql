af3ca4706aa7e67a844cf194e10f5357a9b08719
Lars Francke <lars.francke@gmail.com>
2020-11-09 12:24:34 +0100

    Updated the DB schema

--- db/db.sql
+++ db/db.sql
@@ -1,16 +1,18 @@
-CREATE TABLE cluster_scoped (
-  api_group TEXT,
-  kind TEXT,
+CREATE TABLE cluster_scoped
+(
+  api_group     TEXT,
+  resource_type TEXT,
   resource_name TEXT,
-  json TEXT,
-  PRIMARY KEY (api_group, kind, resource_name)
+  json          TEXT,
+  PRIMARY KEY (api_group, resource_type, resource_name)
 );
 
-CREATE TABLE namespace_scoped (
-  api_group TEXT,
-  kind TEXT,
-  namespace TEXT,
+CREATE TABLE namespace_scoped
+(
+  api_group     TEXT,
+  resource_type TEXT,
+  namespace     TEXT,
   resource_name TEXT,
-  json TEXT,
-  PRIMARY KEY (api_group, kind, namespace, resource_name)
+  json          TEXT,
+  PRIMARY KEY (api_group, resource_type, namespace, resource_name)
 );
